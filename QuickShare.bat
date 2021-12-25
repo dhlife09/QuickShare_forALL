7ba99422d18299e064c99ee22f3ea7d577e2a561
Dohyun Park <dhlife09@gmail.com>
2021-12-25 19:09:07 +0900

    Update QuickShare.bat

--- QuickShare.bat
+++ QuickShare.bat
@@ -1,6 +1,6 @@
 @echo off
 echo [QuickShare] Automatically modifies the registry to enable Samsung QuickShare.
-echo [QuickShare] Copyright 2021 dhlife09. All Rights Reserved.
+echo [QuickShare] https://github.com/dhlife09/QuickShare_forALL
 reg add "HKEY_LOCAL_MACHINE\HARDWARE\DESCRIPTION\SYSTEM\BIOS" /v "BaseBoardManufacturer" /t REG_SZ /d "samsung" /f
 reg add "HKEY_LOCAL_MACHINE\HARDWARE\DESCRIPTION\SYSTEM\BIOS" /v "BaseBoardProduct" /t REG_SZ /d "NT930XDY" /f
 reg add "HKEY_LOCAL_MACHINE\HARDWARE\DESCRIPTION\SYSTEM\BIOS" /v "SystemManufacturer" /t REG_SZ /d "samsung" /f
